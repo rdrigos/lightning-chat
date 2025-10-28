7df2b2fc4c6f38fe245fa92afd9e711dff679b7f
rdrigos <rafaeldrigo.rds@gmail.com>
2025-10-28 20:18:19 -0300

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { AppModule } from '@/app.module';
+import { NestFactory } from '@nestjs/core';
+
+(async () => {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+})();
