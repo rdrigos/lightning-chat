2ba5764e78f31c03636177baa08ac3e5bceb4c9e
Rafael Drigo <rafaeldrigo.rds@gmail.com>
2026-03-16 19:58:11 -0300

    feat(environment): create Environment module for validating and providing environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { AppModule } from '@/app.module';
+import { EnvironmentService } from '@/infrastructure/environment/environment.service';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 (async () => {
+  const logger = new Logger('NestApplication');
+
+  // Create the NestJS application instance
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  // Resolve required services from the DI container
+  const env = app.get(EnvironmentService);
+
+  // Retrieve host and port configuration from environment
+  const host = env.get('HOST');
+  const port = env.get('PORT');
+
+  // Start the HTTP server
+  await app.listen(port, host);
+
+  // Retrieves the application URL
+  const url = await app.getUrl();
+
+  logger.log(`Server is running at: ${url}`);
 })();
